8c61ad30be9945d65b1ca0b0d6707fd903b0799c
Todd Pangilinan <pang.todd@gmail.com>
2022-05-09 12:47:57 -0700

    bugfix, @@total_rows was missing a

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/album')
+require('./lib/song')
 require('pry')
 also_reload('lib/**/*.rb')
 
@@ -44,20 +45,52 @@ get('/albums/:id')do
   #"this route will show a specific album based on its ID. The value of ID here is #{params[:id]}"
 end
 
-post('/albums') do
-  "this route will add an album to our list of albums. We can't access this by typing in the URL. IIn a future lesson, we will use a form that specifies a POST action to reach this route."
-end
-
 get('/albums/:id/edit') do
-  "this will take us to a page with a form for updating an album with an ID of #{params[:id]}."
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
+  #"this will take us to a page with a form for updating an album with an ID of #{params[:id]}."
 end
 
 patch('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.update(params[:name])
+  @albums = Album.all
+  erb(:albums)
   #"this route will update an album. We can't reach it with a URL. In a future lesson, we will use a form that specifices a PATCH action to reach this route"
 end
 
 delete('/albums/:id') do
-  "this route will delete an album. We can't reach it with a URL. In a future lesson, we will use a delete button that specifies a DELETE action to reach this route."
+  @album = Album.find(params[:id].to_i())
+  @album.delete()
+  @albums = Album.all
+  erb(:albums)
+  #"this route will delete an album. We can't reach it with a URL. In a future lesson, we will use a delete button that specifies a DELETE action to reach this route."
+end
+
+post('/albums/:id/songs')do
+  @album = Album.find(params[:id].to_i())
+  song = Song.new(params[:song_name], @album.id, nil)
+  song.save()
+  erb(:album)
+end
+
+get('/albums/:id/songs/:song_id') do
+  @song = Song.find(params[:song_id].to_i())
+  erb(:song)
+end
+
+patch('/albums/:id/songs/:song_id')do
+  @album = Album.find(param[:id].to_i())
+  song = Song.find(params[:song_id].to_i())
+  song.update(params[:name], @album.id)
+  erb(:album)
+end
+
+delete('/albums/:id/songs/:song_id')do
+  song = Song.find(params[:song_id].to_i())
+  song.delete
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
 end
 
 get('/custom_route') do
