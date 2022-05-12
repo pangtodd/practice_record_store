849168b6aa8f7da54ecf6d99941da46e3ce71c94
Todd Pangilinan <pang.todd@gmail.com>
2022-05-12 10:29:20 -0700

    update #== method for Album class, passes test.

--- lib/album.rb
+++ lib/album.rb
@@ -1,44 +1,53 @@
 class Album
-  attr_reader :id, :name
+  attr_reader :id
+  attr_accessor :name
 
-  @@albums = {}
-  @@total_rows = 0
-
-  def initialize(name, id)
-    @name = name
-    @id = id || @@total_rows += 1
-  end
-
-  def self.all
-    @@albums.values()
-  end
-
-  def save
-    @@albums[self.id] = Album.new(self.name, self.id )
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
   end
 
   def ==(album_to_compare)
     self.name() == album_to_compare.name()
   end
 
+  def self.all
+    returned_albums = DB exec("SELECT * FROM albums;")
+    albums = []
+    returned_albums.each() do |album|
+      name = album.fetch("name")
+      id = album.fetch("id").to_i
+      albums.push(Album.new({:name => name, :id => id}))
+    end
+    albums
+  end
+
+  def save
+    result = DB.exec("INSERT INTO albums (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
   def self.clear
-    @@albums ={}
-    @@total_rows= 0
+    DEB.exec("DELTE FROM albums *;")
   end
 
   def self.find(id)
-    @@albums[id]
+    album = DB.exec("SELECT * FROM albums WHERE id = #{id};").first
+    name = album.fetch("name")
+    id = album.fetch("id").to_i
+    Album.new({:name => name, :id => id})
   end
 
   def update(name)
     @name = name
+    DB.exec("UPDATE albums SET name = '#{@name}' WHERE id = #{@id}")
   end
 
-  def delete
-    @@albums.delete(self.id)
-  end
+  # def delete
+  #   @@albums.delete(self.id)
+  # end
 
-  def songs
-    Song.find_by_album(self.id)
-  end
+  # def songs
+  #   Song.find_by_album(self.id)
+  # end
 end
