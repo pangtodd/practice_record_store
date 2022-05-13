7fcbd2e5e47c15247c39034c2473bf45e90c95ce
Todd Pangilinan <pang.todd@gmail.com>
2022-05-13 13:04:00 -0700

    udpate spec_helper to include artists.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,14 @@ require 'pg'
 require 'album'
 require 'song'
 require 'pry'
+require 'artist'
+
+DB = PG.connect({:dbname => 'record_store_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM albums *;")
+    DB.exec("DELETE FROM songs *;")
+    DB.exec("DELETE FROM artists *")
+  end
+end
