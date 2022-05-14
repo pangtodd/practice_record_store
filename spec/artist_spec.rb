44d8f759bd4d7517c9f3529987965c7c1453aef9
Todd Pangilinan <pang.todd@gmail.com>
2022-05-14 16:44:05 -0700

    create new test for update method of Artist class.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -45,11 +45,32 @@ describe 'Artist' do
   end
   describe('#update')do
     it("updates an artist by id")do
-      artist = Artist.new({:name=>'Old Dirty Bastard', :id=>nil})
+      artist = Artist.new({:name=>"Old Dirty Bastard", :id=>nil})
       artist.save()
       artist.update("Big Baby Jesus")
       expect(artist.name).to eq("Big Baby Jesus")
     end
   end
+  describe('#update') do
+    it("adds an album to an artist") do
+      artist = Artist.new({:name => "John Coltrane", :id => nil})
+      artist.save()
+      album = Album.new({:name => "A Love Supreme", :id => nil})
+      album.save()
+      artist.update({:album_name => "A Love Supreme"})
+      expect(artist.albums).to(eq([album]))
+    end
+  end
+  # describe('#delete')do
+  #   it("deletes all songs and albums belonging to a deleted artist????")do
+  #     album = Album.new({:name=>"Coolin in Cali", :id=>nil})
+  #     album.save()
+  #     song = Song.new({:name=>"Goes Like Dis", :album_id=>album.id, :id=>nil})
+  #     song.save()
+  #     artist = Artist.new({:name=>"7a3", :id =>nil})
+  #     artist.delete()
+  #     expect(Album.find(album.id)).to eq(nil)
+  #   end
+  # end
 
 end
